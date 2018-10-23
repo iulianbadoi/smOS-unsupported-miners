abcc59a6a3aeeb8289d16a3b463cb0748e4bedaf
Greerso <danny@greerso.com>
2018-10-22 20:39:06 -0400

    add Ubqminer

--- addons/Ubqminer_ethminer-0.11.0.sh
+++ addons/Ubqminer_ethminer-0.11.0.sh
@@ -4,8 +4,8 @@ GITHUB_BIN_URL="$(curl -sSL https://api.github.com/repos/$1/releases/latest | jq
     curl -sSL "$GITHUB_BIN_URL" | tar xvz --strip-components=1 -C /root/miner_org/$3/
 }
 
-whiptail --msgbox 'Replacing ethminer-v0.11.0 with Ubqminer.' --backtitle 'Ubqminer' --title 'Replacing ethminer-v0.11.0' 15 55
+whiptail --msgbox "Replacing ethminer-v0.11.0 with Ubqminer." --backtitle "Ubqminer" --title "Replacing ethminer-v0.11.0" 15 55
 install_miner "ubiq/ubqminer" "9.1-linux" "ethminer-v0.11.0"
 ln -s --force ubqminer ethminer
 cd
-whiptail --msgbox 'Go to http://simplemining.net and setup a new rig group for ethminer-v0.11.0 that is now actually Ubqminer.  If you have 1080/1080ti You are going to want to enable Ethlargement for this one.' --backtitle 'Ubqminer' --title 'Ubqminer' 15 55
+whiptail --msgbox "Go to http://simplemining.net and setup a new rig group for ethminer-v0.11.0 that is now actually Ubqminer.  If you have 1080/1080ti You are going to want to enable Ethlargement for this one." --backtitle "Ubqminer" --title "Ubqminer" 15 55
