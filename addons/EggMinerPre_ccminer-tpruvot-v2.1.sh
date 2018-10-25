992503ce7adfa8f3eaeb263088909b19ff3bcf17
Greerso <danny@greerso.com>
2018-10-25 14:28:49 -0400

    eggminer pre-release

--- addons/EggMinerPre_ccminer-tpruvot-v2.1.sh
+++ addons/EggMinerPre_ccminer-tpruvot-v2.1.sh
@@ -1,5 +1,6 @@
 install_miner() {
 GITHUB_BIN_URL="$(curl -sSL https://api.github.com/repos/$1/releases/tags/$4 | jq -r ".assets[] | select(.name | test(\"$2\")) | .browser_download_url")"
+	rm -rf /root/miner_org/$3
     mkdir /root/miner_org/$3
     curl -sSL "$GITHUB_BIN_URL" | tar xvz --strip-components=1 -C /root/miner_org/$3/
 }
